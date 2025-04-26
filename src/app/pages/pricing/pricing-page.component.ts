b8235c49ec982fcc30fb5e5addbcd81082ed6d48
Jose P <jopeji@gmail.com>
2025-04-26 00:56:20 -0600

    SSR Enabled

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-pricing',
@@ -7,4 +15,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {}
+export default class PricingPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing page';
+    // }
+
+    // console.log(this.platform);
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Pricing Page',
+    });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing page' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,mundo,Jose,Perez',
+    });
+  }
+}
